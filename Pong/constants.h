7b183e20c1132b0deeea82e29e0f2d575b2f620d
Sean Lucas <smlucas1993@gmail.com>
2017-04-16 09:52:30 -0400

    1st commit
    All of the constants and generally used header files.

---
+++ Pong/constants.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <SDL_mixer.h>
+#include <stdio.h>
+#include <string>
+#include <sstream>
+
+const int SCREEN_WIDTH = 1000;
+const int SCREEN_HEIGHT = 480;
